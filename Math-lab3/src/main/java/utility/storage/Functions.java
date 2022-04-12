f79ddfeacbf1c8b3d02573db9ec4473a1688e6d8
dmittrey <dmitryzubakhin@mail.ru>
2022-04-12 19:05:57 +0300

    [Work-3] Change gap config

--- Math-lab3/src/main/java/utility/storage/Functions.java
+++ Math-lab3/src/main/java/utility/storage/Functions.java
@@ -6,11 +6,9 @@ public enum Functions implements DoubleFunction<Double> {
 
     FUNCTION_ONE(value -> Math.exp(Math.sin(value)), "e^(sin(x))"),
     FUNCTION_TWO(value -> {
-        if (value == 2) return 4.;
-        else return 2.;
-    }, """
-            x = 2, f(x) = 4,
-            x>2, x<2, f(x) = 2"""),
+        if (value != 2) return 4.;
+        else return Double.NaN;
+    }, "x>2, x<2, f(x) = 2"),
     FUNCTION_THREE(value -> Math.abs(1 / (value - 2)), "|1/(x-2)|");
 
     private final DoubleFunction<Double> function;
