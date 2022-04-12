ff83b2e4fda112daf6c2debce61c093ead5d8514
dmittrey <dmitryzubakhin@mail.ru>
2022-04-12 15:35:15 +0300

    [Work-3] Add list consuming feature to simplify filling

--- Math-lab3/src/main/java/dto/Slice.java
+++ Math-lab3/src/main/java/dto/Slice.java
@@ -2,10 +2,17 @@ package dto;
 
 import lombok.Value;
 
+import java.util.List;
+
 @Value
 public class Slice {
     double startPoint;
     double stopPoint;
+
+    public Slice(List<Double> list) {
+        startPoint = list.get(0);
+        stopPoint = list.get(1);
+    }
 
     public double getDist() {
         return stopPoint - startPoint;
