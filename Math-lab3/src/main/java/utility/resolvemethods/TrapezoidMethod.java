30956813f4ba70a500ba91b0bfe4014404db8d25
dmittrey <dmitryzubakhin@mail.ru>
2022-04-12 15:31:37 +0300

    [Work-3] Add bridge first gap

--- Math-lab3/src/main/java/utility/resolvemethods/TrapezoidMethod.java
+++ Math-lab3/src/main/java/utility/resolvemethods/TrapezoidMethod.java
@@ -4,6 +4,8 @@ import dto.Function;
 
 public class TrapezoidMethod implements ResolveMethod {
 
+    private static final double EPSILON = Math.pow(10, -9);
+
     @Override
     public Double resolve(Function function, double countOfSlices, double epsilon) {
 
@@ -11,11 +13,19 @@ public class TrapezoidMethod implements ResolveMethod {
 
         double sumOfFunctionResults = 0;
 
-        for (double currentPoint = function.getSlice().getStartPoint() + stepDist; currentPoint <= function.getSlice().getStopPoint() - stepDist; currentPoint += stepDist)
-            sumOfFunctionResults += function.getOwnFunction().apply(currentPoint);
+        for (double currentPoint = function.getSlice().getStartPoint() + stepDist;
+             currentPoint <= function.getSlice().getStopPoint() - stepDist;
+             currentPoint += stepDist) {
+            sumOfFunctionResults += bridgeGap(function, currentPoint);
+        }
 
-        return stepDist/2 * (function.getOwnFunction().apply(function.getSlice().getStartPoint())
+        return stepDist / 2 * (function.getOwnFunction().apply(function.getSlice().getStartPoint())
                 + function.getOwnFunction().apply(function.getSlice().getStopPoint())
                 + 2 * sumOfFunctionResults);
+    }
+
+    public Double bridgeGap(Function function, double targetPoint) {
+        return (function.getOwnFunction().apply(targetPoint - EPSILON) +
+                function.getOwnFunction().apply(targetPoint + EPSILON)) / 2;
     }
 }
