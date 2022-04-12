504a7057144ddd6f369a2ee82fbb41a85ffecc1a
dmittrey <dmitryzubakhin@mail.ru>
2022-04-12 15:34:22 +0300

    [Work-3] Add functions pool

---
+++ Math-lab3/src/main/java/utility/Functions.java
@@ -0,0 +1,24 @@
+package utility;
+
+import java.util.function.DoubleFunction;
+
+public enum Functions implements DoubleFunction<Double> {
+
+    FUNCTION_ONE(value -> Math.exp(Math.sin(value))),
+    FUNCTION_TWO(value -> {
+        if (value == 2) return 4.;
+        else return 2.;
+    }),
+    FUNCTION_THREE(value -> Math.abs(1 / (value - 2)));
+
+    private DoubleFunction<Double> function;
+
+    Functions(DoubleFunction<Double> function) {
+        this.function = function;
+    }
+
+    @Override
+    public Double apply(double value) {
+        return function.apply(value);
+    }
+}
